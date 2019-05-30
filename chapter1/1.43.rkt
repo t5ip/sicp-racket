580c3f49ec68da80771ba7ea882f0e302b0aa9c2
t5ip <pikaviesti.testitili@gmail.com>
2019-05-31 00:01:39 +0300

    1.43

---
+++ chapter1/1.43.rkt
@@ -0,0 +1,19 @@
+#lang sicp
+
+(define (inc x)
+  (+ x 1))
+
+(define (square x)
+  (* x x))
+
+(define (repeat f x n)
+  (cond ((= 1 n)
+         (f x))
+        (else
+         (repeat f (f x) (- n 1)))))
+
+(define (repeated f n)
+  (lambda (x)
+    (repeat f x n)))
+
+((repeated square 2) 5)
