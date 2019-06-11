9a308bb090cc2327e6423bfe2e6ebb9abf79b945
t5ip <pikaviesti.testitili@gmail.com>
2019-06-11 23:29:38 +0300

    1.45

--- chapter1/1.45.rkt
+++ chapter1/1.45.rkt
@@ -11,6 +11,11 @@
 (define (g x n)
   (lambda (y) (/ x (pow y n))))
 
-(define (n-root x n)
-  (fixed-point (average-damp (g x (- n 1))) 1))
+(define (n-root x n guess)
+  (fixed-point ((repeated average-damp n) (g x (- n 1))) guess))
 
+; Test. Note add .0 to do calculations using floating point arithmetic.
+; Otherwise the calculation will take too long.
+(n-root 65536.0 2 1)
+(n-root 27.0 3 1)
+(n-root 9765625.0 10 1)
