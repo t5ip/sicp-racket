c64624480ab347d9fd1d860be38816e710cbc0d3
t5ip <pikaviesti.testitili@gmail.com>
2019-06-21 00:04:55 +0300

    1.46.rkt

--- chapter1/1.46.rkt
+++ chapter1/1.46.rkt
@@ -2,8 +2,13 @@
 
 (define (iterative-improve good-enough? improve)
   (define (iterate guess x)
-  (cond ((good-enough? guess x) guess)
-        (else (iterate (improve guess x) x))))
+  (cond ((good-enough? guess x)
+         (newline)
+         (display guess)
+         guess)
+        (else (iterate (improve guess x) x)
+              (newline)
+              (display guess))))
   (lambda (guess x) (iterate guess x)))
 
 ; sqrt
@@ -18,13 +23,36 @@
   (average guess (/ x guess)))
 
 (define (good-enough-sqrt? guess x)
-  (< (abs (- (square guess) x)) 0.001))
+  (< (abs (- (square guess) x)) 0.0000001))
 
 (define (sqrt x)
   ((iterative-improve good-enough-sqrt? improve-sqrt) 1.0 x))
 
-;test 
-
+;test
+(newline)
+(display "sqrt 2")
 (sqrt 2)
+(newline)
+(display "sqrt 9")
 (sqrt 9)
+(newline)
+(display "sqrt 10000")
 (sqrt 10000)
+
+; fixed-point
+(newline)
+(display "fixed point sin(x) + cos(x)")
+
+(define (f x)
+  (+ (sin x) (cos x)))
+
+(define (improve-fp guess x)
+  (f guess))
+
+(define (good-enough-fp? guess x)
+  (< (abs (- (f guess) guess)) 0.0000001))
+
+(define (fp guess)
+  ((iterative-improve good-enough-fp? improve-fp) guess guess))
+
+(fp 1)
